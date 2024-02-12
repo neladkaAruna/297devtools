997029db90fc886cea0d9a82dc96176532b1b385
neladkaAruna <153279723+neladkaAruna@users.noreply.github.com>
2024-02-12 11:21:57 +0530

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,26 @@
+const contacts = [
+    {
+      name: "Beyonce",
+      imgURL:
+        "https://tse2.mm.bing.net/th?id=OIP.phEDfgJViJ7EcoccTol8kQHaEq&pid=Api&P=0&h=180",
+      phone: "+123 456 789",
+      email: "b@beyonce.com"
+    },
+    {
+      name: "Jack Bauer",
+      imgURL:
+        "https://tse4.mm.bing.net/th?id=OIP.9Yyo0FAjmWHMTkYXwp8AngHaE8&pid=Api&P=0&h=180",
+      phone: "+987 654 321",
+      email: "jack@nowhere.com"
+    },
+    {
+      name: "Chuck Norris",
+      imgURL:
+        "https://tse2.mm.bing.net/th?id=OIP.DFzbR-cFwmYHdpR1j82H1AHaLH&pid=Api&P=0&h=180",
+      phone: "+918 372 574",
+      email: "gmail@chucknorris.com"
+    }
+  ];
+  
+  export default contacts;
+  
